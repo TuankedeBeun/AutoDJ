64fddb45dfd4a17137ca86616b9c45d2b97d37cd
TuankedeBeun <tuanke@live.nl>
2022-05-04 22:39:53 +0200

    Implemented loading/switching songs

--- analysis/songtester/property_setter.py
+++ analysis/songtester/property_setter.py
@@ -1,9 +1,9 @@
 import os
+import csv
 import tkinter as tk
-from turtle import bgcolor
 import matplotlib.pyplot as plt
 from PIL import Image, ImageTk
-
+from time import localtime
 
 '''
 Classes for setting song properties by listening and manually asserting data
@@ -35,14 +35,24 @@ os.chdir('C:\\Users\\tuank\\Programming\\Python\\AutoDJ\\analysis\\songtester')
 class PropertySetter(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.create_GUI()
 
+        # define properties
+        self.default_music_directory = 'C:/Users/tuank/Music/Drum & Bass/'
         self.song_nr = 0
+        self.song_paths = []
+        self.song_pdb = None
+        self.current_song = {
+            'song_path' : None,
+            'drop_start' : None,
+            'drop_end' : None,
+            'key' : None
+        }
 
+        # create data file
+        self.writer = self.initiate_data_file()
+        self.create_GUI()
         
-    def create_GUI(self, width=1600, height=900):
-        # packing manager: grid
-        
+    def create_GUI(self, width=1200, height=600):
         # colors
         colors = {
             'bg': '#B4C7E7',
@@ -51,13 +61,15 @@ class PropertySetter(tk.Tk):
         }
         
         # initiate window
+        self.geometry("{width}x{height}".format(width = width, height = height))
+        self.resizable(False, False)
         self.title('Song analyser')
         self.rowconfigure(1, weight=2)
         self.configure(background=colors['bg'])
         
         # song title
-        txt_songtitle = tk.Label(text = 'Artist - Song title', bg=colors['bg'], font=('cambria', 50))
-        txt_songtitle.grid(row=0, columnspan=6)
+        self.songtitle = tk.Label(text = 'Artist - Song title', bg=colors['bg'], font=('cambria', 40), wraplength=width-100)
+        self.songtitle.grid(row=0, columnspan=7)
         
         def resize_image(file_path, factor):
             img = Image.open(file_path)
@@ -71,9 +83,9 @@ class PropertySetter(tk.Tk):
 
         # info text
         text = '''
-        Drop start:\t01:02:14
-        Drop end:\t01:54:01
-        Key:\t\tBm'''
+        Drop start:\t00:00:00
+        Drop end:\t00:00:00
+        Key:\t\t-'''
         tk.Label(self, text=text, font=('cambria', 20), bg=colors['bg'], justify=tk.LEFT).grid(row=1, column=5, columnspan=2, sticky=tk.W)
 
         # folder
@@ -115,32 +127,69 @@ class PropertySetter(tk.Tk):
         
         return
 
-    def open_folder(self, event):
+    def initiate_data_file(self):
+        now = localtime()
+        filename = "analysis_{day:2d}{month:2d}{year:4d}-{hour:2d}:{min:2d}.csv"
+        filename_formatted = filename.format(
+            day = now.tm_mday,
+            month = now.tm_mon,
+            year = now.tm_year,
+            hour = now.tm_hour,
+            min = now.tm_min
+            )
+        csv_file = open(filename_formatted, mode='w')
+        writer = csv.writer(csv_file)
+        writer.writerow(["song_path", "drop start", "drop end", "key"])
+        return writer
+
+    def open_folder(self):
+        # load songs in chosen directory
+        music_folder = tk.filedialog.askdirectory(initialdir=self.default_music_directory)
+        songs = os.listdir(music_folder)
+
+        # only keep mp3 songs
+        for song in songs:
+            if (song[-4:] != '.mp3'):
+                songs.remove(song)
+        
+        self.song_paths = songs
+        self.load_song()
+        return
+    
+    def load_song(self):
+        self.current_song['song_path'] = self.song_paths[self.song_nr]
+        self.songtitle.config(text = self.current_song['song_path'][:-4])
+
+    def save_song(self):
+        #TODO: implement check if song is already saved --> overwrite
+        if (None in self.current_song.values()):
+            tk.messagebox.showinfo('Fill properties')
+        else:
+            self.writer.writerow([self.song_paths[self.song_nr]])
+
+    def set_drop_start(self):
         pass
 
-    def save_song(self, event):
+    def set_drop_end(self):
         pass
 
-    def open_song(i):
+    def set_key(self):
         pass
 
-    def set_drop_start(self, event):
+    def open_piano(self):
         pass
 
-    def set_drop_end(self, event):
-        pass
-
-    def set_key(self, event):
-        pass
-
-    def open_piano(self, event):
-        pass
-
-    def previous_song(self, event):
-        pass
-
-    def next_song(self, event):
-        pass
+    def previous_song(self):
+        #TODO: load song if already in csv file
+        if (self.song_nr > 0):
+            self.song_nr -= 1
+            self.load_song()
+    
+    def next_song(self):
+        #TODO: load song if already in csv file
+        if (self.song_nr < len(self.song_paths) - 1):
+            self.song_nr += 1
+            self.load_song()
 
     def play(self, event):
         pass
