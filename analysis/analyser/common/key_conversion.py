b03b7aae3384bbb31e060d93a637fe1018f9ac95
TuankedeBeun <tuanke@live.nl>
2023-02-19 20:30:26 +0100

    Added circle of fifths conversion

--- analysis/analyser/common/key_conversion.py
+++ analysis/analyser/common/key_conversion.py
@@ -1,13 +1,23 @@
-tones = ['A','Bb','B','C','C#','D','Eb','E','F','F#','G','G#']
+TONES = ['A','Bb','B','C','C#','D','Eb','E','F','F#','G','G#']
+CIRCLE_OF_FIFTHS_MAJOR = ['E','B','F#','C#','G#','Eb','Bb','F','C','G','D','A']
+CIRCLE_OF_FIFTHS_MINOR = ['C#','G#','Eb','Bb','F','C','G','D','A','E','B','F#']
 
 def from_key_to_keynumber(key):
-    key_number = tones.index(key.strip('m'))
+    key_number = TONES.index(key.strip('m'))
 
     return key_number
 
 def from_keynumber_to_key(key_number, is_major):
-    key = tones[key_number]
+    key = TONES[key_number]
     if not is_major:
         key += 'm'
 
     return key
+
+def from_key_to_circle_of_fifths(key):
+    if key.endswith('m'):
+        circle_number = CIRCLE_OF_FIFTHS_MINOR.index(key.strip('m'))
+    else:
+        circle_number = CIRCLE_OF_FIFTHS_MAJOR.index(key)
+
+    return circle_number
